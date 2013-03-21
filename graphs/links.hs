1b460373eb26e6805a55bcb769569e9f646ceb71
Stefan Kaestle <stefan.kaestle@inf.ethz.ch>
2013-03-21 12:37:18 +0100

    Links for gruyere

--- graphs/links.hs
+++ graphs/links.hs
@@ -1,6 +1,30 @@
 import Debug.Trace
 
-links = [(1,2),(1,4),(2,3)]
+--------------------------------------------------
+-- Links
+--------------------------------------------------
+
+links_bidirectional = [
+  (1,2),
+  (1,5),
+  (2,3),
+  (2,6),
+  (3,4),
+  (3,8),
+  (3,7),
+  (4,8),
+  (4,7),
+  (5,6),
+  (6,7),
+  (7,8)
+  ]
+                      
+links_unidirectional =                      
+  links_bidirectional ++ reverse
+  where
+    reverse = map (\ (s,e) -> (e,s)) links_bidirectional
+    
+links = links_bidirectional  -- deprecated
 
 --------------------------------------------------
 -- Logic
@@ -21,7 +45,7 @@ linkCost (x,y)
                     | otherwise                 = trace ("calculating minimum for list " ++ show nb_costs) minimum nb_costs -- get minimum of these costs
     nb_costs = trace ("calculating nb cost for " ++ show nb_list) (map linkCost nb_list) -- get cost of these links
     nb_list = trace ("getting nbs reached by links " ++ show nb ) (map (\ (s,e) -> (e,y)) nb) -- prepare input tuples for recursive call
-    nb = filter ((==x).fst) links -- find links to neighbors
+    nb = filter ((==x).fst) links_unidirectional -- find links to neighbors
 
 main :: IO()
 main = print (linkCost (1,3))
@@ -42,5 +66,5 @@ linkCostTest (x,y) = print nb_list
 
 
 test :: IO()
-test = (linkCostTest (1,3))
+test = print (length links_unidirectional)
 
