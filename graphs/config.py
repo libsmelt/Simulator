239d9a7c2a8127d79b33413078fd90069ff682a4
Stefan Kaestle <stefan.kaestle@inf.ethz.ch>
2013-05-02 18:07:34 +0200

    Added scheduling by longest path

--- graphs/config.py
+++ graphs/config.py
@@ -1,3 +1,4 @@
 # This is meant to hold all sort of configuration options
 
 SCHEDULING_SORT=False
+SCHEDULING_SORT_LONGEST_PATH=True
--- graphs/evaluate.py
+++ graphs/evaluate.py
@@ -2,11 +2,14 @@ import events
 import heapq
 import draw
 import config
+import pdb
 
 # Evaluation is round-based
 
 # Assumptions:
 # 1) nodes never have to process different events at the same time
+
+# XXX It would probably better to have a directed graph!
 
 round = 0
 event_queue = []
@@ -83,20 +86,63 @@ def receive_cost(node):
     """
     return 10
 
+def _get_longest_path_for_node(node, omit, depth):
+    """
+    Return the length of the longest path starting at the given node
+    Note: Graph must not have cycles (is this true?)
+    """
+    assert not omit == None
+    l = 0
+    for n in model.neighbors(node):
+        if not n in omit: 
+            o_ = omit
+            o_.append(node)
+            ltmp = model.edge_weight((node, n)) + \
+                _get_longest_path_for_node(n, o_, depth+1)
+            indent = ''
+            for i in range(depth):
+                indent += ' '
+            print "%sPath length from %d via %d is %d" % (indent, node, n, ltmp)
+            l = max(l, ltmp)
+    return l
+
+def get_longest_path(src, omit):
+    """
+    Return the maximum length for subtrees starting at every node
+    given in nblist
+    """
+    paths = []
+
+    assert not omit == None
+    omit.append(src)
+
+    print "=================================================="
+    for n in model.neighbors(src):
+        if not n in omit:
+            m = model.edge_weight((src, n)) + \
+                _get_longest_path_for_node(n, omit, 0)
+            paths.append((m, n))
+    return paths
+
 def send(src, cost, omit):
     """
     Simulate sending a message from given node.
     No message will be send towards nodes given in omit
-    XXX Scheduling decision here
     """
+    assert isinstance(omit, list)
     send_time = cost
     nb = []
     # Create list of children first, and the cost of the message list
     for dest in model.neighbors(src):
         if not dest in omit:
             nb.append((model.edge_weight((src, dest)), dest))
+    # Find longest path for all neighbors
+    if config.SCHEDULING_SORT_LONGEST_PATH:
+        nb = get_longest_path(src, omit)
+        for (nbc, nbn) in nb:
+            print "longest path from node %d via %d is %d\n" % (src, nbn, nbc)
     # Sort this list
-    if config.SCHEDULING_SORT:
+    if config.SCHEDULING_SORT or config.SCHEDULING_SORT_LONGEST_PATH:
         nb.sort(key=lambda tup: tup[0], reverse=True)
     # Walk the list and send messages
     for (cost, dest) in nb:
