766839beecaaa91f122f6cc43a6eedd64abef753
Stefan Kästle <stefan.kaestle@inf.ethz.ch>
2013-09-28 11:31:36 +0200

    Added missing files

--- overlays_RMME.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2007, 2008, 2009, 2010, 2011, 2012, 2013 ETH Zurich.
-
-class Overlay:
-    """
-    Base class for finding the right overlay topology for a model
-    """
-    def get_broadcast_tree(self):
-        return None
-
