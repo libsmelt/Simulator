50e066311c68fa1fb98140a6ce9aaa4209d24eba
Stefan Kaestle <stefan.kaestle@inf.ethz.ch>
2016-04-27 20:43:09 +0200

    scripts: added script to simulate all machines

---
+++ simulate_all.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import sys
+import os
+sys.path.append(os.getenv('HOME') + '/bin/')
+
+import subprocess
+import tools
+
+def main():
+    import config
+
+    sys.path.append(config.MACHINE_DATABASE)
+    from machineinfo import machines
+
+    out = []
+
+    for s in [ s for (s, _, _) in machines ]:
+        cmd = ['./simulator.py', s, 'adaptivetree']
+
+        res = []
+
+        try:
+            for l in subprocess.check_output(cmd, stderr=subprocess.STDOUT).split('\n'):
+
+                if l.startswith('Cost'):
+                    e = l.split(':')[1].split()
+                    res.append(int(e[0]))
+
+            bla = (s, res[0], res[1], res[2])
+            out.append(bla)
+            print '%-30s %6d %6d %6d' % bla
+
+        except subprocess.CalledProcessError:
+            print s, 'failed'
+        except:
+            raise
+
+
+if __name__ == "__main__":
+    main()
