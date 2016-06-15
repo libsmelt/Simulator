a60c63c925c99a5510da4d05f3b900f1e90fa346
Stefan Kaestle <stefan.kaestle@inf.ethz.ch>
2016-06-15 11:44:17 +0200

    visu: changed graph topology colors

--- helpers.py
+++ helpers.py
@@ -74,7 +74,12 @@ def output_clustered_graph(graph, name, clustering):
 
     A = nx.to_agraph(G)
 
-    clist = [ 'red', 'green', 'blue', 'orange', 'grey', 'yellow' ]
+    tableau20 = [ '#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78',
+                  '#2ca02c', '#98df8a', '#d62728', '#ff9896',
+                  '#9467bd', '#c5b0d5', '#8c564b', '#c49c94',
+                  '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7',
+                  '#bcbd22', '#dbdb8d', '#17becf', '#9edae5' ]
+    clist = [ tableau20[i*2] for i in range(0, len(tableau20)/2)]
 
     i = 0
     for c in clustering:
@@ -597,4 +602,3 @@ class bcolors:
 
 def warn(msg):
     print bcolors.WARNING + msg + bcolors.ENDC
-
