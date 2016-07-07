11530901147e8d20b3985b4cb1412c3da0de60f0
Stefan Kaestle <stefan.kaestle@inf.ethz.ch>
2016-07-07 16:02:19 +0200

    mm: removed old code + generate mm matrix w/ factors

--- multimessage.py
+++ multimessage.py
@@ -18,7 +18,7 @@ from topology_parser import on_same_node
 class MultiMessage(object):
 
     @staticmethod
-    def parse(f, topology, mode_list):
+    def parse(f, topology, mode_list=[ 'last', 'sum' ]):
         """Parse the given MultiMessage benchmark file
 
         """
@@ -33,7 +33,7 @@ class MultiMessage(object):
 
         tsc_overhead = -1            # << TSC overhead read from  output
 
-        for line in f:
+        for line in f.readlines():
 
             # Format:
             # cores=01,02,03,04,08,12,16,20,24,28, mode=all , avg=109, stdev=13, med=106, min=98, max=355 cycles, count=1800, ignored=3200
@@ -103,115 +103,49 @@ class MultiMessage(object):
         return (sender_core, cores_local, cores_remote, tsc_overhead, data, err, history)
 
 
-    def __init__(self, _input=sys.stdin, mmtype='last'):
+    def __init__(self, _input, machine, mmtype='last'):
         """Initiate the multimessage parser.
 
-        @param mmtype: Choose the type of multimessage to use. Supported are:
-            'last', 'all'"""
+        @param Instance of the machine to create the multimessage benchmark for
+        @param mmtype: Choose the type of multimessage to use. Supported are: 'last', 'all'
 
-        # Initialize
-        self.res = [[ None for j in range(MAX) ] for i in range(MAX) ]
+        """
 
-        num_read = 0
-        self.max_local = 0
-        self.max_remote = 0
+        self.machine = machine
 
-        # Parse input lines
-        for line in _input:
-            m = re.match('r=(\d+),l=(\d+), mode=([^,]+), avg=(\d+), stdev=(\d+), med=(\d+), min=(\d+), max=(\d+) cycles, count=(\d+), ignored=(\d+)', line)
-            if m :
-                l = int(m.group(2))
-                r = int(m.group(1))
-                mode = m.group(3).rstrip()
+        tmp = MultiMessage.parse(_input, machine.machine_topology)
+        self.sender_core, self.cores_local, self.cores_remote, \
+            self.tsc_overhead, self.data, self.err, self.history = tmp
 
-                if mode != mmtype:
+        self.history = self.history['sum']
+
+        self.init_matrix()
+
+    def init_matrix(self):
+        """Initiliaze the multimessage matrix.
+
+        This is a "correction" matrix. For each multimessage
+        configuration of sending r remote and l local messages,
+        calculate how much the cost of the same send history using
+        n-receive is off.
+
+        """
+
+        for r in range(self.cores_remote)[::-1]:
+            print '>> ',
+            for l in range(self.cores_local):
+
+                if r==0 and l==0:
+                    print '                         |',
                     continue
 
-                rem, loc = tuple([ int(m.group(i)) for i in [1, 2]])
-                avg, stdev, med = tuple([ float(m.group(i)) for i in [4, 5, 6]])
+                # Determine send cost as predicted by n-receive
+                send_pw = self.machine.get_send_history_cost(self.sender_core, self.history[r][l])
+                rel_error = send_pw/float(self.data['sum'][r][l])
 
-                # Sanity checks
-                _check = abs(1.0-avg/med)
-                if _check>0.1:
-                    helpers.warn('WARNING: avg/med more than 10% apart ' + str(_check) + ' for ' + str((rem, loc)))
+                cost_last = self.data['last'][r][l] - self.tsc_overhead
 
-                _check = stdev/avg
-                if _check>0.1:
-                    helpers.warn('WARNING: avg/med more than 10% apart ' + str(_check) + ' for ' + str((rem, loc)))
+                print ' %5.1f %5.0f %5.0f %5.2f |' % \
+                    (cost_last, self.data['sum'][r][l], send_pw, rel_error),
 
-                assert self.res[rem][loc] == None # otherwise we would have several
-                                             # results for the same data
-                self.res[rem][loc] = (avg, stdev, med)
-                self.max_local = max(self.max_local, loc)
-                self.max_remote = max(self.max_remote, rem)
-                num_read += 1
-
-    def get_factor(self, num_remote, num_local, local=True):
-        """@param local Whether the message to be sent now (and who's cost to
-        look up) is local
-
-        """
-        # Add the new message (whos type is indicated by local parameter)
-        if local:
-            num_local += 1
-        else:
-            num_remote += 1
-
-        if num_remote > self.max_remote and num_local == 0:
-            cost = self.res[self.max_remote][1][0]
-        else:
-            num_remote = min(self.max_remote, num_remote)
-            cost = self.res[num_remote][num_local][0]
-
-        return cost
-
-
-
-
-    def get_factor_old(self, num_remote, num_local, local=True):
-        """@param local Whether the message to be sent now (and who's cost to
-        look up) is local
-
-        """
-
-        # The measurements are the accumulated send cost; we are
-        # interested in the difference to the previous history,
-        # i.e. if we are now sending message n(k), we are taking
-        # n(1) .. n(k-1), take the difference of that and compare
-        # it to n(1), the cost of sending just one message, which
-        # is encoded in the pairwise results
-        prev = self.res[num_remote][num_local][0] if \
-               num_remote != 0 or num_local != 0 else 0
-
-        # Add the new message (whos type is indicated by local parameter)
-        if local:
-            num_local += 1
-        else:
-            num_remote += 1
-
-        if self.res[num_remote][num_local] == None :
-            print "num_remote=%d, num_local=%d" % (num_remote, num_local)
-        assert not self.res[num_remote][num_local] == None
-
-        # Get baseline
-        baseline = self.res[0][1] if local else self.res[1][0]
-
-        # Cost when adding new send operation
-        nxt = self.res[num_remote][num_local][0]
-        last_snd_cost = nxt - prev # cost of the additional, new
-                                   # message, not yet accounted for
-
-        # XXX Have warning here that pops up when the std error is too high
-
-        if last_snd_cost>0:
-            return (last_snd_cost) / baseline[0]
-
-        else:
-            # This is a weird case from broken measurement: the cost
-            # of sending one more message is than SMALLER than the
-            # cost of sending all the messages in the history apart
-            # from the last one. I.e. sending one more message makes
-            # the cost SMALLER - we treat this by just returning a
-            # factor 1.0
-            helpers.warn('multimessage: send_cost < 0 - using factor of 1.0')
-            return 1.0
+            print ''
