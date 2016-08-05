1f1f8e35ab08dde55c8c6f9878cc33265b320afc
Stefan Kaestle <stefan.kaestle@inf.ethz.ch>
2016-08-05 19:12:41 +0200

    vulture: added whitelist.py file to prevent false-positives

---
+++ whitelist.py
@@ -0,0 +1,17 @@
+# Whitelist file to prevent false-positives in Vulture
+
+import bruteforce
+bruteforce.bfThread.run()
+
+import helpers
+helpers.bcolor.HEADER
+helpers.bcolor.UNDERLINE
+
+import evaluate
+evaluate.NodeState.send_batch
+
+import hybrid_model
+hybrid_model.HybridModule.parent
+
+import simulator
+simulator.sys.excepthook
